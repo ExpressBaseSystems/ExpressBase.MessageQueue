8cabb1de430b07ecb27a2580c2cc159bde27b700
DonajoseUllattil <donajose@expressbase.com>
2018-10-15 17:39:01 +0530

    Email Connection Modification

--- Services/Workers/RefreshSolutionConnections.cs
+++ Services/Workers/RefreshSolutionConnections.cs
@@ -73,6 +73,7 @@ namespace ExpressBase.MessageQueue.MQServices
                     if (dt.Rows.Count != 0)
                     {
                         EbSmsConCollection _smscollection = new EbSmsConCollection();
+                        EbMailConCollection _mailcollection = new EbMailConCollection();
                         foreach (DataRow dr in dt.Rows)
                         {
                             if (dr["con_type"].ToString() == EbConnectionTypes.EbDATA.ToString())
@@ -99,8 +100,9 @@ namespace ExpressBase.MessageQueue.MQServices
                             }
                             else if (dr["con_type"].ToString() == EbConnectionTypes.SMTP.ToString())
                             {
-                                cons.SMTPConnection = EbSerializers.Json_Deserialize<SMTPConnection>(dr["con_obj"].ToString());
-                                cons.SMTPConnection.Id = (int)dr["id"];
+                                EbEmail temp = EbSerializers.Json_Deserialize<EbEmail>(dr["con_obj"].ToString());
+                                temp.Id = (int)dr["id"];
+                                _mailcollection.Add(temp);
                             }
                             else if (dr["con_type"].ToString() == EbConnectionTypes.SMS.ToString())
                             {
@@ -120,6 +122,7 @@ namespace ExpressBase.MessageQueue.MQServices
                             }// ... More to come
                         }
                         cons.SMSConnections = _smscollection;
+                        cons.EmailConnection = _mailcollection;
                         Redis.Set<EbConnectionsConfig>(string.Format(CoreConstants.SOLUTION_CONNECTION_REDIS_KEY, req.SolnId), cons);                       
                     }
                 }
