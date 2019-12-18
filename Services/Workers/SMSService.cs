85e803acdeba4be54e993239d0bcd52188675209
Josevin <josevin@expressbase.com>
2019-12-18 12:52:14 +0530

    Query Modified for file service

--- Services/Workers/SMSService.cs
+++ Services/Workers/SMSService.cs
@@ -52,7 +52,10 @@ namespace ExpressBase.MessageQueue.MQServices
         {
             EbConnectionFactory connectionFactory = new EbConnectionFactory(request.SolnId, this.Redis);
 
-            string sql = connectionFactory.DataDB.EB_SMSSERVICE_POST;
+            string sql = @"INSERT INTO logs_sms
+                            (uri, send_to, send_from, message_body, status, error_message, user_id, context_id) 
+                        VALUES 
+                            (@uri, @to, @from, @message_body, @status, @error_message, @user_id, @context_id) RETURNING id;";
 
             DbParameter[] parameters =
                     {
